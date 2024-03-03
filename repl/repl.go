d0838b9c47a16b0e351c3f45e738e07cc408cc15
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-03-03 12:49:36 +0530

    update repl to print parsed result

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/divxvid/monkey-interpreter/lexer"
-	"github.com/divxvid/monkey-interpreter/token"
+	"github.com/divxvid/monkey-interpreter/parser"
 )
 
 const PROMPT = ">>> "
@@ -24,9 +24,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserError(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserError(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
