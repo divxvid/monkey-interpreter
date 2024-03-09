66008eeace9499cdf503053facd0c9c6f2a4973d
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-03-09 21:40:49 +0530

    Add evaluator for boolean infix expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -26,7 +26,7 @@ func Eval(node ast.Node) object.Object {
 			Value: node.Value,
 		}
 	case *ast.Boolean:
-		return nativeToBooleanObj(node.Value)
+		return nativeBoolToBooleanObj(node.Value)
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
@@ -49,7 +49,7 @@ func evalStatements(statements []ast.Statement) object.Object {
 	return result
 }
 
-func nativeToBooleanObj(input bool) *object.Boolean {
+func nativeBoolToBooleanObj(input bool) *object.Boolean {
 	if input {
 		return TRUE
 	}
@@ -95,6 +95,12 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		//this works because we are using singletons for TRUE, FALSE and NULL
+		//so they will refer to the same object hence the equality
+		return nativeBoolToBooleanObj(left == right)
+	case operator == "!=":
+		return nativeBoolToBooleanObj(left != right)
 	default:
 		return NULL
 	}
@@ -113,6 +119,14 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBoolToBooleanObj(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBooleanObj(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObj(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObj(leftVal != rightVal)
 	default:
 		return NULL
 	}
