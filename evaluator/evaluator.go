72567dc6050a6787d00e5869752fa9709c8fced5
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-03-09 15:23:12 +0530

    Add boolean evaluation logic

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -18,6 +18,10 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{
 			Value: node.Value,
 		}
+	case *ast.Boolean:
+		return &object.Boolean{
+			Value: node.Value,
+		}
 	}
 
 	return nil
