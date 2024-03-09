9ecd2341034f140fa023a3350f2773d08b36617c
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-03-09 16:01:01 +0530

    Implement NOT prefix operator evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -27,6 +27,9 @@ func Eval(node ast.Node) object.Object {
 		}
 	case *ast.Boolean:
 		return nativeToBooleanObj(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -47,4 +50,26 @@ func nativeToBooleanObj(input bool) *object.Boolean {
 		return TRUE
 	}
 	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
