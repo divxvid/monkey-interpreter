885c3c19d3fc16326adc3f74291da47a35f779e0
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-03-09 22:01:44 +0530

    Add evaluator for if-else expressions

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/divxvid/monkey-interpreter/lexer"
@@ -41,6 +42,7 @@ func testEval(input string) object.Object {
 	p := parser.New(l)
 
 	program := p.ParseProgram()
+	fmt.Printf("input: %s, parsed: %s\n", input, program.String())
 
 	return Eval(program)
 }
@@ -124,4 +126,38 @@ func TestBangOperator(t *testing.T) {
 		evaluated := testEval(tt.input)
 		testBooleanObject(t, evaluated, tt.expected)
 	}
+}
+
+func TestIfElseExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"if (true) { 10 }", 10},
+		{"if (false) { 10 }", nil},
+		{"if (1) { 10 }", 10},
+		{"if (1 < 2) { 10 }", 10},
+		{"if (1 > 2) { 10 }", nil},
+		{"if (1 > 2) { 10 } else { 20 }", 20},
+		{"if (1 < 2) { 10 } else { 20 }", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Errorf("object is not NULL. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	return true
 }
