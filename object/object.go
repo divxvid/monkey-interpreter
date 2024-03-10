df053d2a3db02ca2ed952fef08c9cbfc63cf64b5
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-03-10 13:29:29 +0530

    Add object for tracking Return values

--- object/object.go
+++ object/object.go
@@ -8,7 +8,8 @@ type Object interface {
 }
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
