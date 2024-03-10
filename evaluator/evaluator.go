a2af428c8848925a793f38fc05da72dc7d5a6a26
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-03-10 13:41:24 +0530

    Implement return from nested blocks

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -16,11 +16,11 @@ func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	//Statements
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatements(node)
 	case *ast.ReturnStatement:
 		val := Eval(node.ReturnValue)
 		return &object.ReturnValue{Value: val}
@@ -46,10 +46,10 @@ func Eval(node ast.Node) object.Object {
 	return nil
 }
 
-func evalStatements(statements []ast.Statement) object.Object {
+func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
 
-	for _, statement := range statements {
+	for _, statement := range program.Statements {
 		result = Eval(statement)
 
 		if returnValue, ok := result.(*object.ReturnValue); ok {
@@ -57,6 +57,23 @@ func evalStatements(statements []ast.Statement) object.Object {
 			//this stops the execution of the statements after return
 			//and returns the return value
 			return returnValue.Value
+		}
+	}
+
+	return result
+}
+
+func evalBlockStatements(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			//if we encounter any return statement inside blocks, we just return
+			//without evaluating the next statements.
+			//value is passed as it is to be handled by the evalProgram
+			return result
 		}
 	}
 
